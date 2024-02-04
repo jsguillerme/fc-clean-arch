2b2e0c8652bce929be6ec79404f2e4d5a35d66ab
Guilherme Andrade <guilhermegm.andrade@gmail.com>
2024-02-04 15:17:31 -0300

    test: ensure notification pattern

---
+++ src/domain/@shared/notification/notification.spec.ts
@@ -0,0 +1,77 @@
+import Notification from "./notification";
+
+describe('Unit test for notification', () => {
+
+  it('should create errors', () => {
+    const notification = new Notification();
+    const error = {
+      message: 'error message',
+      context: 'customer'
+    };
+
+    notification.addError(error);
+
+    expect(notification.messages('customer')).toBe('customer: error message,');
+
+    const error2 = {
+      message: 'error message2',
+      context: 'customer'
+    };
+
+    notification.addError(error2);
+
+    const error3 = {
+      message: 'error message3',
+      context: 'order'
+    };
+
+    notification.addError(error3);
+
+    expect(notification.messages('customer')).toBe('customer: error message,customer: error message2,');
+    expect(notification.messages()).toBe('customer: error message,customer: error message2,order: error message3,');
+  });
+
+  it('should check if notification has errors', () => {
+    const notification = new Notification();
+    const error = {
+      message: 'error message',
+      context: 'customer'
+    };
+
+    notification.addError(error);
+
+    expect(notification.hasErrors()).toBe(true);
+  });
+
+  it('should check if notification has no errors', () => {
+    const notification = new Notification();
+
+    expect(notification.hasErrors()).toBe(false);
+  });
+
+  it('should get all errors props', () => {
+    const notification = new Notification();
+    const error = {
+      message: 'error message',
+      context: 'customer'
+    };
+
+    notification.addError(error);
+
+    const error2 = {
+      message: 'error message2',
+      context: 'customer'
+    };
+
+    notification.addError(error2);
+
+    const error3 = {
+      message: 'error message3',
+      context: 'order'
+    };
+
+    notification.addError(error3);
+
+    expect(notification.errors()).toEqual([error, error2, error3]);
+  });
+});
